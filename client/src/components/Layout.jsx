5335efa7284f3b60849971734564d622979b8c93
IEBS15 <ayush.kumar@iebrain.com>
2024-09-16 12:37:09 +0530

    dockerized

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,46 +1,32 @@
-import React from 'react'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import { Separator } from "@/components/ui/separator"
+import React from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import { Separator } from "@/components/ui/separator";
 
-function Layout({children}) {
+function Layout({ children }) {
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col h-screen">
+      {/* Sticky Header */}
+      <div className="sticky top-0 z-50">
         <Header />
-        <div className='flex flex-row'>
-            <Sidebar />
-            <Separator orientation="vertical" className='bg-black' />
-            <div className='flex flex-col'>
-                {children}
-            </div>
+      </div>
+      
+      <div className="flex flex-row flex-1 overflow-hidden">
+        {/* Sticky Sidebar */}
+        <div className="sticky top-0 h-screen">
+          <Sidebar className="h-full bg-gray-800 text-white" />
         </div>
 
+        {/* Vertical Separator */}
+        <Separator orientation="vertical" className="bg-black" />
+
+        {/* Main Content Area */}
+        <div className="flex-1 overflow-auto p-4">
+          {children}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-
-
-
-
-
-// import React from 'react';
-// import Header from './Header';
-// import Sidebar from './Sidebar';
-
-// function Layout({ children }) {
-//   return (
-//     <div className="flex flex-col h-screen overflow-hidden">
-//         <Header className="sticky top-0 w-full bg-gray-100 shadow-md" />
-//       <div className="flex flex-row flex-1">
-//       <Sidebar className="fixed top-0 left-0 h-full bg-gray-800 text-white" />
-//         <div className="flex-1 overflow-auto p-4">
-//           {children}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 export default Layout;
- 
